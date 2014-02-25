0767d9cbc85efb69e0790d13c22e8f1db3c8949e
Petter Solås <petter.solaas@gmail.com>
2014-02-25 13:28:07 +0100

    third commit test... this time with push to upstrean

--- src/Keyword/PoiFetcher.java
+++ src/Keyword/PoiFetcher.java
@@ -31,7 +31,7 @@ public class PoiFetcher {
 		/*
 		 * To do:
 		 * 
-		 * - Set up git/svn - working on it - still working
+		 * - Set up git/svn - working on it - still working -third test
 		 * - see if owl properties can be used
 		 * - add direct property query to the current subject
 		 * - start work on android
